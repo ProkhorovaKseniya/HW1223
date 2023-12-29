7b85c7c5eeddcab3fcf84bba0f994d172cfc36ef
Kseniya <kseniya_pro@gmx.net>
2023-12-29 22:48:12 +0600

    formated task1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,15 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-//Console.WriteLine("Hello, World!");
-// ****
-// Задача 1: Напишите программу, которая принимает на вход число
+﻿// Задача 1: Напишите программу, которая принимает на вход число
 // и проверяет, кратно ли оно одновременно 7 и 23.
 //****
 Console.Write("Введите число:");
-int number = Convert.ToInt32(Console.ReadLine());
-// Console.WriteLine(number);
+int number = Convert.ToInt32(Console.ReadLine()); //ввод числа типа int
+
 if (number%7==0 && number%23==0) {
     Console.WriteLine($"Введенное число {number} кратно одновременно 23 и 7");
-
 }
 else
 {
