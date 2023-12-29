16f9eaac93dbbe238be15609a1686e45eeacbdb9
Kseniya <kseniya_pro@gmx.net>
2023-12-29 23:51:51 +0600

    Completed Task2

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 2: Напишите программу, которая принимает на вход координаты точки (X и Y),
+// причём X ≠ 0 и Y ≠ 0 
+//и выдаёт номер координатной четверти плоскости, в которой находится эта точка.
+Console.Write("Введите координаты Х:");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.Write("  и Y:");
+int y = Convert.ToInt32(Console.ReadLine());
+if (x>0) {
+    if (y>0) {
+        Console.Write($"Координаты точки ({x};{y}) находятся в I четверти");
+    } 
+    else {
+        Console.Write($"Координаты точки ({x};{y}) находятся в IV четверти");
+    }
+}
+else {
+   if (y>0) {
+        Console.Write($"Координаты точки ({x};{y}) находятся в II четверти");
+    } else {
+        Console.Write($"Координаты точки ({x};{y}) находятся в III четверти");
+    }  
+}     
+
