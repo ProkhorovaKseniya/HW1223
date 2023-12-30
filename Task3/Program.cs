58d8d57f93e03ee289d35a9288bea2dcb4cd7df6
Kseniya <kseniya_pro@gmx.net>
2023-12-31 00:58:47 +0600

    Task3 done on branch Task3

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -1,2 +1,18 @@
-﻿// // See https://aka.ms/new-console-template for more information
-// Console.WriteLine("Hello, World!");
+﻿// Задача 3: Напишите программу, которая принимает на вход 
+// целое число из отрезка [10, 99] и показывает наибольшую цифру числа.
+
+Console.Write("Введите целое число из отрезка [10, 99]");
+int number=Convert.ToInt32(Console.ReadLine());
+if  (number>9 && number<100) {
+
+    if ((number/10)>(number%10)) {
+    Console.WriteLine($"Наибольшая цифра числа {number} - {number/10}");
+    }
+    else {
+        if ((number/10)==(number%10)) {
+            Console.WriteLine($"Во введенном числе {number} обе цифры равны  - {number%10}");
+        }
+        else {Console.WriteLine($"Наибольшая цифра числа {number} - {number%10}");}
+    }
+}
+else {Console.WriteLine($"Введенное число {number} вне заданного отрезка [10, 99]");}
