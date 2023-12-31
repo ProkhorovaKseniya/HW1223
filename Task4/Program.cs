526dad6ba31f50c80a7aea9a160286156c69dad6
Kseniya <kseniya_pro@gmx.net>
2023-12-31 10:21:06 +0600

    finished Task4 on the main branch

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -1,5 +1,20 @@
 ﻿// Задача 4: Напишите программу, которая на вход принимает натуральное число N, 
 // а на выходе показывает его цифры через запятую. 
 // Допускается вывод цифр в обратном порядке, например: 3462 => 2,6,4,3
-Console.Write("Введите натуралььное число");
-long N=Convert.
+Console.Write("Введите натуралььное число: ");
+int N=Convert.ToInt32(Console.ReadLine());
+if (N < 10){
+Console.WriteLine(N);
+}
+else {
+    while (N>0) {
+        int digit=N%10;
+        N/=10;
+        if (N>0) {
+            Console.Write(digit+", ");
+        }
+        else{
+            Console.Write(digit);
+        }
+    }   
+}
